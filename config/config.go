fd64c1033847f5fe273cbbf2765fb625f474f726
suke <suke971219@gmail.com>
2020-01-15 09:12:35 +0800

    数据库配置删除

--- config/config.go
+++ config/config.go
@@ -4,6 +4,6 @@ var (
 	//SiteName 网址名称
 	SiteName string = "Go test web service"
 	//DataBase 数据库配置 username:password@host/database_name?param
-	// DataBase string = "testgo:G3ZmecsKyGT2kpwC@(www.abadboy.cn:3306)/testgo?charset=utf8"
+	// DataBase string = "testgo:123234@(www.123.cn:3306)/testgo?charset=utf8"
 	DataBase string = "root:2568597007suke@(localhost:3306)/test?charset=utf8mb4&parseTime=true&loc=Asia%2fShanghai"
 )
