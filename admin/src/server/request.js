cf7dbb54c14e34ee556af7cc37cce40de3e19a67
suke <suke971219@gmail.com>
2020-01-22 13:17:42 +0800

    url config

--- admin/src/server/request.js
+++ admin/src/server/request.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import custom from './custom'
+import config from '../config'
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8080',
+    baseURL: config.baseURL,
     timeout:2500,
     // validateStatus:custom.err
 });
