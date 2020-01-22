04030bdcb6c8c7e6dd224af97e2bed94116c3a01
suke <suke971219@gmail.com>
2020-01-22 18:49:41 +0800

    更新build shell自动拷贝静态文件

---
+++ build.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+# 创建文件夹
+mkdir -p  dist/{config,h5,static,template}
+echo '编译项目中..'$1
+export TempDir=./dist
+export GOOS=''
+export File=main
+while [ -n "$1" ]  
+do  
+  case "$1" in   
+    -linux)  
+        echo "发现 -a 选项 编译目标：$1"  
+        GOOS=linux
+        File=linux-main
+        ;;  
+    -win)  
+        echo "发现 -a 选项 编译目标：$1"  
+        GOOS=windows
+        File=main.exe
+        ;;
+    *)  
+        echo "$1 is not an option,由go build选择当前默认编译平台"  
+        ;;  
+  esac  
+  shift  
+done
+echo CGO_ENABLED=0 GOOS=$GOOS GOARCH=amd64 go build -o $TempDir/$File
+eval CGO_ENABLED=0 GOOS=$GOOS GOARCH=amd64 go build -o $TempDir/$File
+
+
+# 拷贝静态资源和配置文件
+echo ' - 拷贝配置文件...'
+cp -r config/config.json $TempDir/config/config.json
+echo ' - 拷贝前端目录...'
+cp -r h5/* $TempDir/h5
+echo ' - 拷贝资源文件...'
+cp -r static/* $TempDir/static
+echo ' - 拷贝模板文件...'
+cp -r template/* $TempDir/template
+echo '完成...'
