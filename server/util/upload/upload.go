59b6ed7330a09d2394fd4cbf1f482809fe5c9fe7
suke <2568597007@qq.com>
2020-05-11 18:40:42 +0800

    独立上传方法

---
+++ server/util/upload/upload.go
@@ -0,0 +1,134 @@
+package upload
+
+import (
+	"EK-Server/util"
+	"errors"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/labstack/echo"
+)
+
+var (
+	// AcceptsImgExt  图片类型
+	AcceptsImgExt = []string{"jpg", "png", "jpeg", "webp", "gif"} //图片类型
+	// AcceptsVideoExt 视频类型
+	AcceptsVideoExt = []string{"mov", "mp4", "avi"} //视频类型
+	//AcceptsOtherFileExt 文件类型
+	AcceptsOtherFileExt = []string{"pdf", "zip", "rar", "gz", "txt"} //其他文件类型
+)
+
+// Default 默认上传类型和文件夹
+func Default(c echo.Context) error {
+	return Custom(c, []string{}, "")
+}
+
+// Custom 自定义上传类型和目录
+func Custom(c echo.Context, acceptsExt []string, folder string) error {
+	file, err := c.FormFile("file")
+	if err != nil {
+		return util.JSONErr(c, err, "上传错误") //未获取到文件流
+	}
+	link, err := uploadBase(file, acceptsExt, folder)
+	if err != nil {
+		fmt.Println(err)
+		errMsg := fmt.Sprintf("%s", err)
+		if errMsg == "" {
+			errMsg = "上传失败"
+		}
+		return util.JSONErr(c, nil, errMsg)
+	}
+	return util.JSONSuccess(c, link, "上传成功")
+}
+
+func uploadBase(file *multipart.FileHeader, acceptsExt []string, folderName string) (fileName string, err error) {
+	pathExt := path.Ext(file.Filename)
+
+	folder := ""
+	// 如果符合类型，设定目录
+	if inArray(AcceptsImgExt, strings.Trim(pathExt, ".")) {
+		folder = "image"
+	}
+	if inArray(AcceptsVideoExt, strings.Trim(pathExt, ".")) {
+		folder = "video"
+	}
+	if inArray(AcceptsOtherFileExt, strings.Trim(pathExt, ".")) {
+		folder = "file"
+	}
+	// 自定义类型  覆盖前边的
+	if inArray(acceptsExt, strings.Trim(pathExt, ".")) {
+		folder = folderName
+	}
+	// 如果不符合任何一种类型
+	if folder == "" {
+		err = errors.New("文件不合法")
+		return
+	}
+
+	// 打开文件流
+	src, err := file.Open()
+	if err != nil {
+		err = errors.New("打开文件失败")
+		return
+
+	}
+	defer src.Close() //函数结束时自动关闭文件
+
+	//创建文件夹
+	dir, err := getDir("./static/upload/"+folder+"/", time.Now().Format("2006_01_02"))
+	if err != nil {
+		err = errors.New("创建文件夹失败")
+		return
+	}
+
+	// 随机文件名 + 文件后缀
+	randName := util.RandStringBytes(32) + pathExt
+	// Destination
+	fileName = filepath.Join(dir, randName)
+
+	// 创建空文件
+	dst, err := os.Create(fileName)
+	if err != nil {
+		err = errors.New("创建文件失败")
+		return
+	}
+	defer dst.Close()
+	// Copy文件流到新建到文件
+	if _, err = io.Copy(dst, src); err != nil {
+		err = errors.New("拷贝文件至目标失败")
+		return
+	}
+	// 拼接文件地址，不带协议头，方便处理http 到https升级 ， 其实也没找到协议头在哪儿，req对象里没有返回到空字符串
+	fileName = fmt.Sprintf("/%s", fileName)
+	return
+}
+
+// 创建文件夹
+func getDir(path string, foderName string) (dir string, err error) {
+	folder := filepath.Join(path, foderName)
+	if _, err = os.Stat(folder); os.IsNotExist(err) {
+		err = os.MkdirAll(folder, os.ModePerm)
+		if err != nil {
+			return
+		}
+	}
+	dir = folder
+	return
+}
+
+// 在数组中
+func inArray(arr []string, item string) (inArr bool) {
+	index := -1
+	for i, x := range arr {
+		if item == x {
+			index = i
+		}
+	}
+	return index > -1
+}
