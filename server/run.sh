7d09396fb1d9811959e458764e64ebf85357836a
suke <2568597007@qq.com>
2020-07-21 16:29:10 +0800

    重启脚步

--- server/run.sh
+++ server/run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+tmp_path=""
 
-pid=$(ps -ef |grep ./debug_bin |grep -v grep | awk '{print $2}');
-echo "查找运行中 ./debug_bin 的进程..."
+if [ ! -n "$1" ]
+then
+  echo "请输入程序路径 like ./debug_bin"
+  exit
+fi
+
+tmp_path=$1
+
+pid=$(ps -ef |grep $tmp_path |grep -v grep | grep -v 'run.sh' |  awk '{print $2}');
+echo "查找运行中 $tmp_path 的进程..."
 
 if [ ! -n "$pid" ]
 then
@@ -14,7 +23,7 @@ else
     done
 fi
 echo "building..."
-go build -o debug_bin .
+go build -o $tmp_path
 echo "restarting..."
-nohup ./debug_bin >> ./log/nohup.log &
+nohup $tmp_path >> ./log/nohup.log &
 echo 'done.'
