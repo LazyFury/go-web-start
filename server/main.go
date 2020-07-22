57227904c4f856c44776e0d651bb9f51997e1cbe
suke <2568597007@qq.com>
2020-07-22 11:53:44 +0800

    自动部署测试

--- server/main.go
+++ server/main.go
@@ -69,7 +69,7 @@ func main() {
 // requestInfo
 func requestInfo(c echo.Context) error {
 	req := c.Request()
-	format := "<pre><strong>Request Information test</strong>\n\n<code>Protocol: %s\nHost: %s\nRemote Address: %s\nMethod: %s\nPath: %s\n</code></pre>"
+	format := "<pre><strong>Request Information test auto build</strong>\n\n<code>Protocol: %s\nHost: %s\nRemote Address: %s\nMethod: %s\nPath: %s\n</code></pre>"
 	fmt.Println(strings.Split(req.Header.Get("Accept"), ",")[0])
 	fmt.Printf("%+v", req.Header)
 	return c.HTML(http.StatusOK, fmt.Sprintf(format, req.Proto, req.Host, req.RemoteAddr, req.Method, req.URL.Path))
