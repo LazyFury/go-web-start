cc5224b80b501d6245986670b4b9eae7595be7bb
suke <2568597007@qq.com>
2020-09-04 17:36:13 +0800

    fix:run shell

--- server/run.sh
+++ server/run.sh
@@ -27,14 +27,14 @@ then
 
   echo "restarting..."
   # nohup  $tmp_path >> ./log/nohup.log &
-  exec $tmp_path >> ./log/nohup.log 2>&1 & 
+  exec $tmp_path >> ./log/nohup.log & #2>&1 & 
   #通知重启一下  结束当前回话 nohup有些异常
   exec $0 $1 #linux only
 else
   for i in `echo $pid`
     do
       echo "kill 进程pid $i 通知原进程fork子进程"
-      kill -s SIGHUP $i &
+      kill -s 1 $i;
     done
 fi
 
