20b4c66a7de542c629252cb9c2ea17fd22e96bfa
suke <2568597007@qq.com>
2020-09-07 21:58:33 +0800

    fix:启用hash路由

--- admin/.umirc.ts
+++ admin/.umirc.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'umi';
 
-export default defineConfig({
-  title: '后台管理',
-  nodeModulesTransform: {
-    type: 'none',
-    exclude: [],
-  },
-  // ssr: {
-  //   // mode: 'stream',
-  // },
-  // routes: [{ path: '/', component: '@/pages/index' }],
-});
+export default {
+  ...defineConfig({
+    title: '后台管理',
+    nodeModulesTransform: {
+      type: 'none',
+      exclude: [],
+    },
+    history: { type: 'hash' },
+    // ssr: {
+    //   // mode: 'stream',
+    // },
+    // routes: [{ path: '/', component: '@/pages/index' }],
+  }),
+};
