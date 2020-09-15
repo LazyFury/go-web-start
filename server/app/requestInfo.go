7183e0e6ea77ab1d7b77549243ece25791d3ce6a
suke <2568597007@qq.com>
2020-09-15 14:02:43 +0800

    fix:realIP

--- server/app/requestInfo.go
+++ server/app/requestInfo.go
@@ -26,5 +26,5 @@ func requestInfo(c echo.Context) error {
 	`
 	// fmt.Println(strings.Split(req.Header.Get("Accept"), ",")[0])
 	// fmt.Printf("%+v", req.Header)
-	return c.HTML(http.StatusOK, fmt.Sprintf(format, req.Proto, req.Host, req.RemoteAddr, req.Method, req.URL.Path))
+	return c.HTML(http.StatusOK, fmt.Sprintf(format, req.Proto, req.Host, c.RealIP(), req.Method, req.URL.Path))
 }
