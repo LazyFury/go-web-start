d58dc90238a0ff54b9e29b22974fe8173fb2c942
suke <2568597007@qq.com>
2020-09-15 13:21:24 +0800

    fix:反斜杠重定向仅get请求

--- server/app/app.go
+++ server/app/app.go
@@ -17,22 +17,24 @@ import (
 // New 初始化
 func New() *echo.Echo {
 	e := echo.New() //echo实例
-
-	e.Pre(slash)                                                                   //反斜杠处理
-	e.Use(middleware.Gzip())                                                       //gzip压缩
-	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{Output: os.Stdout})) //日志
-	// e.Use(midd.LogMiddleware())
-	e.Use(middleware.Recover())        //错误处理
-	e.Use(middleware.NonWWWRedirect()) //跳转到没有www到顶级域名
-	e.Use(middleware.Secure())         //安全
-
-	e.Use(session.Middleware(sessions.NewCookieStore([]byte(util.RandStringBytes(32))))) //session
-	e.Use(sessionInit)
 	//跨域
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	e.Pre(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins:     []string{"*"},
 		AllowCredentials: true,
 	}))
+	e.Pre(middleware.NonWWWRedirect()) //跳转到没有www到顶级域名
+	e.Pre(middleware.Secure())         //安全
+	e.Pre(slash)                       //反斜杠处理
+
+	// response
+	e.Use(middleware.Gzip())                                                       //gzip压缩
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{Output: os.Stdout})) //日志
+	// e.Use(midd.LogMiddleware())
+	e.Use(middleware.Recover()) //错误处理
+
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte(util.RandStringBytes(32))))) //session
+	e.Use(sessionInit)
+
 	// fix：csrf未验证crash之后造成之后的cosr配置未生效，已修改顺序
 	// fix：跨域的情况下不建议使用csrf
 	// tips:csrf使用场景，服务端渲染模版的时候，将csrf key自动渲染到页面表单中随数据提交，再跨域到情况下没有比较安全的方案获取到csrf key
--- server/app/slash.go
+++ server/app/slash.go
@@ -1,34 +1,38 @@
 package app
 
 import (
-	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/labstack/echo/v4"
 )
 
+// 处理反斜杠的中间件
 func slash(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		req := c.Request()
 		url := req.URL
 		path := url.Path
-		fmt.Printf("\n%v\n", c)
 		uri := path
 
+		// 是否需要重定向url
 		redirect := false
 
+		// 重复的斜杠，前端拼接url可能造成的异常
 		if strings.Contains(path, "//") {
 			uri = strings.ReplaceAll(path, "//", "/")
 			redirect = true
 		}
 
+		// 删除末尾的斜杠，限制api前缀
+		// 静态文件夹会自动添加后缀，与这里造成重复，造成无限♾️301重定向
 		if strings.HasPrefix(uri, "/api") && strings.HasSuffix(uri, "/") {
 			uri = strings.TrimRight(uri, "/")
 		}
 
 		// redirect
-		if redirect {
+		if redirect && req.Method == http.MethodGet {
+			// 拼接query
 			qs := c.QueryString()
 			if qs != "" {
 				uri += "?"
@@ -40,7 +44,6 @@ func slash(next echo.HandlerFunc) echo.HandlerFunc {
 		url.Path = uri
 		req.URL = url
 		req.RequestURI = uri
-		fmt.Printf("\n%v\n", c)
 
 		return next(c)
 	}
