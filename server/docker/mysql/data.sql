1aed375b6bc5d9b5dd16329e444345c54d203af9
suke <2568597007@qq.com>
2020-09-24 18:25:39 +0800

    fix:mysql charset 支持emoji

--- server/docker/mysql/data.sql
+++ server/docker/mysql/data.sql
@@ -2,6 +2,6 @@
 create user "suke"@"%%" identified by "";
 grant all privileges on *.* to "suke"@"%%";
  DROP database IF EXISTS `test`;
-create database `test` default character set utf8 collate utf8_general_ci; 
+create database `test` default character set utf8mb4 collate utf8mb4_unicode_ci; 
 -- 切换到test_data数据库
 use test; 
