02e423a26539921e0b5211f5689f149fe1803fb5
suke <2568597007@qq.com>
2020-09-27 17:03:02 +0800

    撤回

--- server/start/main.go
+++ server/start/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/Treblex/go-echo-demo/server/app"
 	"github.com/Treblex/go-echo-demo/server/config"
 	"github.com/Treblex/go-echo-demo/server/model"
 	"github.com/Treblex/go-echo-demo/server/router"
+	"github.com/Treblex/go-echo-demo/server/util/mlog"
+
+	"github.com/fvbock/endless"
 )
 
 func main() {
@@ -24,12 +28,12 @@ func main() {
 	// 注册路由
 	router.Start(e)
 
-	e.Logger.Error(e.Start(fmt.Sprintf(":%d", config.Global.Port)))
-	// server := endless.NewServer(fmt.Sprintf(":%d", config.Global.Port), e)
-	// err := server.ListenAndServe()
-	// if err != nil {
-	// 	mlog.Error(err.Error())
-	// }
-	// mlog.Error("stopd\n")
-	// os.Exit(0)
+	// e.Logger.Error(e.StartTLS(fmt.Sprintf(":%d", config.Global.Port), "cert.pem", "key.pem"))
+	server := endless.NewServer(fmt.Sprintf(":%d", config.Global.Port), e)
+	err := server.ListenAndServe()
+	if err != nil {
+		mlog.Error(err.Error())
+	}
+	mlog.Error("stopd\n")
+	os.Exit(0)
 }
