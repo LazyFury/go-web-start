f5a63d9a743b830b6aa750fe8b050bb0fe7efd46
suke <2568597007@qq.com>
2020-09-27 16:41:20 +0800

    fix:start shell

--- server/start/run.sh
+++ server/start/run.sh
@@ -24,19 +24,17 @@ echo "查找运行中 $tmp_path 的进程..."
 if [ ! -n "$pid" ]
 then
   echo "没有运行中的进程";
-
-  echo "restarting..."
+  # echo "restarting..."
   # nohup  $tmp_path >> ./log/nohup.log &
-  exec $tmp_path >> ./log/nohup.log & #2>&1 & 
+  # exec $tmp_path >> ./log/nohup.log & #2>&1 & 
   #通知重启一下  结束当前回话 nohup有些异常
-  exec $0 $1 #linux only
+  # exec $0 $1 #linux only
 else
   for i in `echo $pid`
     do
       echo "kill 进程pid $i 通知原进程fork子进程"
-      kill -s 1 $i;
+      kill -s 9 $i;
     done
 fi
-
-
+exec $tmp_path >> ./log/nohup.log & #2>&1 & 
 echo 'done.'
