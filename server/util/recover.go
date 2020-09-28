726baa1c8eef0be27b143cff62481f3b878a2416
suke <2568597007@qq.com>
2020-09-28 09:53:54 +0800

    add:微信网页sdk测试

--- server/util/recover.go
+++ server/util/recover.go
@@ -1,14 +1,12 @@
 package util
 
-import (
-	"fmt"
-)
+import "github.com/Treblex/go-echo-demo/server/util/mlog"
 
 // Recover 使用defer调用阻止painc中止程序
 func Recover() {
 	defer func() {
 		if r := recover(); r != nil {
-			fmt.Printf("recover拦截:%v\n\n", r)
+			mlog.Printf("recover拦截:%v\n\n", r)
 		}
 	}()
 }
