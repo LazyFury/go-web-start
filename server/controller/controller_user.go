bf7281afb9cd04758085c1c72c6ed18bbe461190
suke <2568597007@qq.com>
2021-04-13 18:44:13 +0800

    登录接口

--- server/controller/controller_user.go
+++ server/controller/controller_user.go
@@ -3,12 +3,16 @@ package controller
 import (
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lazyfury/go-web-start/server/config"
 	"github.com/lazyfury/go-web-start/server/middleware"
 	"github.com/lazyfury/go-web-start/server/model"
 	"github.com/lazyfury/go-web-template/controller"
 	"github.com/lazyfury/go-web-template/response"
+	"github.com/lazyfury/go-web-template/tools/types"
 )
 
 type UserController struct {
@@ -49,10 +53,16 @@ func (u *UserController) Login(c *gin.Context) {
 		response.Error(response.JSON(response.InvalidJSONData, "", err))
 	}
 
-	if err := user.Validator(); err != nil {
-		response.Error(err)
+	// check
+	user.Name = strings.Trim(user.Name, " ")
+	if user.Name == "" {
+		response.Error(response.JSONError("用户名不可空", nil))
+	}
+	if user.Password == "" {
+		response.Error(response.JSONError("用户密码不可空", nil))
 	}
 
+	// 用昵称查找
 	var find = &model.User{}
 	if err := u.DB.GetObjectOrNotFound(find, map[string]interface{}{
 		"name": user.Name,
@@ -60,6 +70,8 @@ func (u *UserController) Login(c *gin.Context) {
 		response.Error(response.JSON(response.NotFound, "用户不存在", nil))
 	}
 
+	// 比对密码
+	user.Password = config.Global.Sha1.EnCode(user.Password)
 	if find.Password != user.Password {
 		response.Error(response.JSON(response.AuthedError, "用户密码错误", nil))
 	}
@@ -104,4 +116,6 @@ func (u *UserController) updateUserInfo(c *gin.Context, user *model.User, status
 	user.IP = c.ClientIP()
 	user.Ua = c.Request.UserAgent()
 	user.Status = status //数据库忘记default了
+	user.LoginTime = types.LocalTime{Time: time.Now()}
+
 }
